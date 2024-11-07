d70c5741609b313efd8cdd69067639353b108451
Natalia Novikova <novikova.nm@phystech.edu>
2024-11-07 17:22:10 +0300

    Упрощенный вариант решения задачи с перемешиванием символов в текстовых блоках и итоговым восстановлением

--- mixing_symbols_two_way_solution.py
+++ mixing_symbols_two_way_solution.py
@@ -1,70 +1,79 @@
+"""
+Написать программу, которая перемешивает символы исходного
+сообщения в блоках по 7 символов и совершает обратное
+преобразование.
+"""
 import random
 
 
-
-def shuffle_symbols(string):
-    shuffled_string = ''
-    # Разделить строку на 128 блоков. Если блоку не хватает 128 символов до его
-    # конца, дополнить его пробелами или плюсами, чтобы было видно на дебаге, до 128 символов
-    blocks_each_128_symbols = generate_symbol_blocks_from_string(string)
-    origin_index_and_symbol_list = generate_list_symbols_and_their_places_in_origin_text(blocks_each_128_symbols)
-   # var = len(origin_index_and_symbol_list)
-    array_of_indexes = list(range(len(origin_index_and_symbol_list)))
-    # Перемешиваем индексы
-    random.shuffle(array_of_indexes)
-    for index in array_of_indexes:
-        shuffled_string += origin_index_and_symbol_list[index][1]
-
-    return shuffled_string, origin_index_and_symbol_list
-
-
-def generate_list_symbols_and_their_places_in_origin_text(blocks_each_128_symbols):
-    current_symbol_index = 0
-    origin_index_and_symbol_list = []
-    for block in blocks_each_128_symbols:
-        for symbol in block:
-            origin_index_and_symbol_list.append((current_symbol_index, symbol))
-            current_symbol_index += 1
-
-    return origin_index_and_symbol_list
+def shuffle_symbols(text):
+    shuffled_text = ""
+    block_size = 7
+    # Нам нужно создать блоки текста корректного размера, как сказано в условии задачи:
+    blocks_correct_size = generate_text_blocks_with_valid_size(block_size, text)
+    indexes_array = list(range(len(blocks_correct_size) * block_size))
+    # Перемешиваем индексы в случайном порядке. Будет создан новый список с перемешанными индексами:
+    shuffled_numbers = random.sample(indexes_array, len(indexes_array))
+    # Создаем пустой массив символов для хранения перемешанного текста:
+    shuffled_symbols_array = [''] * len(indexes_array)
+    # Перебираем все блоки текста:
+    index = 0
+    for block in blocks_correct_size:
+        # Перебираем все символы в блоке:
+        for each_symbol in block:
+            # Добавляем символ в перемешанное сообщение:
+            shuffled_symbols_array[shuffled_numbers[index]] = each_symbol
+            index += 1
+    # Перебираем все символы в перемешанном тексте:
+    for i in range(len(shuffled_symbols_array)):
+        # Добавляем символ в перемешанное сообщение:
+        shuffled_text += shuffled_symbols_array[i]
+    return shuffled_text, indexes_array, shuffled_numbers
 
 
-def generate_symbol_blocks_from_string(string):
-    blocks_with_correct_size = []
-    block_size = 128
-    blocks = [string[i:i + block_size] for i in range(0, len(string), block_size)]
-    # Лист блоков по 128 символов.
-    # Нужно проверить, все ли блоки размером по 128
-    # Если не все - Дополнить строку в листе пробелами до 128
-    for each_block in blocks:
-        each_block_len = len(each_block)
-        while len(each_block) < block_size:
-            each_block += ' '
-        blocks_with_correct_size.append(each_block)
-    return blocks_with_correct_size
+def generate_text_blocks_with_valid_size(block_size, text):
+    # Создаем список с блоками символов по 7 символов в каждом блоке.
+    # Блоки в нашем случае являются маленькими строками
+    # Если блоку до нужного размера не хватает символа, то добавляем блоку пробел в его конец:
+    blocks_correct_size = []
+    # здесь изначально строка текста делится на блоки, но размер последнего блока может быть меньше указанного в задаче
+    blocks = [text[i:i + block_size] for i in range(0, len(text), block_size)]
+    # Если размер блока меньше 7 - "дописываем" пробел в конец блока,
+    # и так, пока количество символов в блоке не станет равным 7
+    for block in blocks:
+        while len(block) < block_size:
+            block += ' '
+        blocks_correct_size.append(block)
+        # Иначе просто добавляем блок в список
+    return blocks_correct_size
 
 
-def unshuffle_symbols(string_after_shuffle, origin_index_and_symbol_list):
-    size = len(string_after_shuffle)
-    array_of_chars = [""] * size
-    correct_index = 0
-    for each_symbol in string_after_shuffle:
-       for index, symbol in origin_index_and_symbol_list:
-            if each_symbol == symbol:
-                correct_index = index
-                break
-       if array_of_chars[correct_index] != '':
-           continue
-       array_of_chars[correct_index] = each_symbol
-    unshuffled_string = "".join(array_of_chars)
-    return unshuffled_string
+def unshuffle_symbols(shuffled, indexes_array, shuffled_numbers):
+    unshuffled_text = ""
+    index = 0
+    # Создаем пустой массив символов для хранения восстановленного текста
+    list_unshuffled_symbols = [''] * len(indexes_array)
+    # Перебираем все символы в перемешанном тексте:
+    for i in range(len(list_unshuffled_symbols)):
+        # Вычисляем какой символ в перемешанном тексте соответствует какому символу в исходном
+        value_for_list = shuffled[index]
+        # Какой индекс в перемешанном тексте соответствует таковому в исходном
+        index_for_list = shuffled_numbers.index(index)
+        # Добавляем символ в исходный текст в его "законное" место
+        list_unshuffled_symbols[index_for_list] = value_for_list
+        index += 1
+        # Перебираем все символы в исходном тексте
+    for i in range(len(list_unshuffled_symbols)):
+        # Делаем строку с восстановленным тестом, избавляемся от пробелов в конце строки
+        unshuffled_text += list_unshuffled_symbols[i]
+    return unshuffled_text.strip()
 
 
 if __name__ == '__main__':
-    string_for_shuffle = "Попробуем снова перемешать символы" * 8
-    print("Исходное сообщение:", string_for_shuffle)
-    string_after_shuffle, origin_index_and_symbol_list = shuffle_symbols(string_for_shuffle)
-    print("Перемешанное сообщение:", string_after_shuffle)
-    # test :print(len(string_for_shuffle)==len(string_after_shuffle))
-    unshuffled_string = unshuffle_symbols(string_after_shuffle, origin_index_and_symbol_list)
-    print("Восстановленное сообщение:", unshuffled_string )
+    text = "Hello, world!"
+    print("Исходное сообщение:", text)
+    shuffled, indexes, shuffled_numbers = shuffle_symbols(text)
+    print("Перемешанное сообщение:", shuffled)
+    original = unshuffle_symbols(shuffled, indexes, shuffled_numbers)
+    print("Восстановленное сообщение:", original)
+    print(text == original)
